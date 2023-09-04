916aa9ec5356d18089414bef049af702a7f41b65
matthieu <matthieu.portelli@gmail.com>
2023-09-04 17:03:02 +0200

    update constantes.py (add model & functions)

--- constantes.py
+++ constantes.py
@@ -1,5 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression, Ridge
+from sklearn.neighbors import KNeighborsClassifier
 
 STRUCTURE = {
     'regression': {
@@ -10,7 +11,14 @@ STRUCTURE = {
                 'copy_X': [True, False],
             }
         },
-        'Regression_Test': {
+        'Régression_Ridge': {
+            'model': Ridge(),
+            'hyperparameters': {
+                'alpha': [0.1, 1.0, 10.0],
+                'fit_intercept': [True, False],
+                'normalize': [True, False],
+                'solver': ['auto', 'svd', 'cholesky', 'lsqr', 'sparse_cg', 'sag', 'saga'],
+            }
         },
     },
     'classification': {
@@ -23,7 +31,45 @@ STRUCTURE = {
                 'min_samples_split': [2, 5, 10]
             }
         },
-        'Classification_Test': {
-        }
+        'KNN_Classification': {
+            'model': KNeighborsClassifier(),
+            'hyperparameters': {
+                'n_neighbors': [3, 5, 10],
+                'weights': ['uniform', 'distance'],
+            }
+        },
+        'Regression_Logistique': {
+            'model': LogisticRegression(),
+            'hyperparameters': {
+                'penalty': ['l1'],  # Utilisez 'l1' comme pénalité
+                'solver': ['liblinear'],  # Spécifiez le solver 'liblinear'
+                'C': [0.1, 1.0, 10.0],
+            }
+        },
     }
 }
+
+
+def get_algo_reg():
+    list_algo_regression = []
+    for model_name, model_info in STRUCTURE['regression'].items():
+        list_algo_regression.append(model_name)
+        print("voici la liste reg", list_algo_regression)
+    return list_algo_regression
+
+
+def get_algo_class():
+    list_algo_classification = []
+    for model_name, model_info in STRUCTURE['classification'].items():
+        list_algo_classification.append(model_name)
+        print("voici la liste class", list_algo_classification)
+    return list_algo_classification
+
+
+def get_hyperparameters(model_name):
+    if model_name in STRUCTURE['regression']:
+        return STRUCTURE['regression'][model_name]['hyperparameters']
+    elif model_name in STRUCTURE['classification']:
+        return STRUCTURE['classification'][model_name]['hyperparameters']
+    else:
+        return {}
