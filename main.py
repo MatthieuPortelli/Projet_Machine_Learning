3efacd2aa44f034d0cbc2e8bf832427a937de121
matthieu <matthieu.portelli@gmail.com>
2023-09-05 17:16:06 +0200

    update: func sidebar

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 import db
 import constantes
-
 from preprocess import DataPreprocessor
-from models import train_model
+from models import train_model, evaluate_model
 
 
 def intro():
@@ -36,50 +34,68 @@ def sidebar():
         with st.expander("Description du DataFrame"):
             description = df.describe()
             st.write(description)
-        selected_model = get_algo(target_type)
+        selected_model, model_family = get_algo(target_type)
+        # Nettoyez les données en utilisant DataPreprocessor
+        data_preprocessor = DataPreprocessor(df, target_column='target')
+        processed_dataframe = data_preprocessor.processed_data
+        X_train = data_preprocessor.X_train
+        X_test = data_preprocessor.X_test
+        y_train = data_preprocessor.y_train
+        y_test = data_preprocessor.y_test
+        with st.expander("Matrice de Corrélation"):
+            correlation_matrix = processed_dataframe.corr()
+            plt.figure(figsize=(10, 8))
+            sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
+            # Ne pas afficher le message warning
+            st.set_option('deprecation.showPyplotGlobalUse', False)
+            st.pyplot()
         with st.expander("GridSearchCV"):
-            # Nettoyez les données en utilisant DataPreprocessor
-            data_preprocessor = DataPreprocessor(df, target_column='target')
-            X_train = data_preprocessor.X_train
-            y_train = data_preprocessor.y_train
             # Récupération des résultats de GridSearchCV
             best_model, best_params, best_score = train_model(selected_model, X_train, y_train)
             st.write(best_model)
             st.write(best_params)
             st.write(best_score)
-        # with st.expander("Matrice de Corrélation"):
-        #     correlation_matrix = df.corr()
-        #     plt.figure(figsize=(10, 8))
-        #     sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
-        #     # Ne pas afficher le message warning
-        #     st.set_option('deprecation.showPyplotGlobalUse', False)
-        #     st.pyplot()
+        with st.expander("Métriques"):
+            metrics = evaluate_model(best_model, X_test, y_test, model_family)
+            st.write(metrics)
+            # for i in metrics.keys():
+            #     if i == 'Accuracy':
+            #         st.write('1')
+            #     elif i == 'Precision':
+            #         st.write('2')
+            #     elif i == 'Recall':
+            #         st.write('3')
+            #     elif i == 'F1-Score':
+            #         st.write('4')
+        with st.expander("Visualisations"):
+            st.write('Des beaux graphiques')
 
 
 def get_algo(target_type):
     # Proposition des algorithmes selon le type de dataset
+    selected_algo, model_family = None, None
     if target_type == "int64":
+        model_family = 'regression'
         print("C'est une régression")
         list_algo_regression = constantes.get_algo_reg()
         selected_algo = st.sidebar.selectbox("Sélectionnez un algorithme", list_algo_regression)
-        if selected_algo:
-            print(f"Vous avez choisi {selected_algo}")
-            get_params(selected_algo)
-            return selected_algo
     elif target_type == "object":
+        model_family = 'classification'
         print("C'est une classification")
         list_algo_classification = constantes.get_algo_class()
         selected_algo = st.sidebar.selectbox("Sélectionnez un algorithme", list_algo_classification)
-        if selected_algo:
-            print(f"Vous avez choisi {selected_algo}")
-            get_params(selected_algo)
-            return selected_algo
+    if selected_algo and model_family:
+        print(f"Vous avez choisi {selected_algo}")
+        get_params(selected_algo)
+        return selected_algo, model_family
 
 
 def get_params(selected_algo):
-    on = st.sidebar.toggle("Modifier la taille de l'entraînement")
+    st.sidebar.markdown('---')
+    on = st.sidebar.toggle("Modifier la taille de test", help="Valeur par default = 0.20")
     if on:
-        print("Ok on modifie")
+        test_size = st.sidebar.slider(" ", 0.0, 1.0, 0.2, 0.05)
+        print("Vous avez choisi test_size =", test_size)
     st.sidebar.markdown('---')
     on = st.sidebar.toggle("Modifier les hyperparamètres")
     if on:
@@ -87,16 +103,16 @@ def get_params(selected_algo):
         hyperparameters = constantes.get_hyperparameters(selected_algo)
         selected_params = {}
         for parametre, values in hyperparameters.items():
-            print(parametre, values)
-            if all(isinstance(value, bool) for value in values):
-                print("isinstance: bool")
-                selected_value = st.sidebar.selectbox(parametre, options=values, key=parametre)
-            elif all(isinstance(value, (int, float)) for value in values):
+            print("test", parametre, values)
+            if all(isinstance(value, (int, float)) for value in values):
                 print("isinstance: int, float")
-                selected_value = st.sidebar.select_slider(parametre, options=values, key=parametre)
+                selected_value = st.sidebar.select_slider(parametre, options=values['values'], key=parametre, help=values['description'])
+            elif all(isinstance(value, bool) for value in values):
+                print("isinstance: bool")
+                selected_value = st.sidebar.selectbox(parametre, options=values['values'], key=parametre, help=values['description'])
             else:
                 print("isinstance: else (str)")
-                selected_value = st.sidebar.selectbox(parametre, options=values, key=parametre)
+                selected_value = st.sidebar.selectbox(parametre, options=values['values'], key=parametre, help=values['description'])
             # Ajouter le paramètre au dictionnaire
             selected_params[parametre] = selected_value
             print("Paramètres sélectionnés :", selected_params[parametre])
