3a2dc631c04003a402436e4c8774c65b18dddbaa
matthieu <matthieu.portelli@gmail.com>
2023-09-06 12:47:59 +0200

    update: dynamic test_size

--- models.py
+++ models.py
@@ -3,7 +3,9 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, precision_score, recall_score, f1_score
 
 
-def train_model(model, X_train, y_train, hyperparameters):
+def train_model(model, X_train, y_train, hyperparameters=None):
+    print('models.py / train_model / hyperparameters1: ', hyperparameters)
+
     if model in STRUCTURE['regression']:
         model_info = STRUCTURE['regression'][model]
     elif model in STRUCTURE['classification']:
@@ -11,6 +13,13 @@ def train_model(model, X_train, y_train, hyperparameters):
     else:
         raise ValueError("Modèle non trouvé dans la structure.")
     model = model_info['model']
+
+    # Récupération des hyperparameters
+    if not hyperparameters:
+        print('TEST')
+        hyperparameters = {param: param_info['values'] for param, param_info in model_info['hyperparameters'].items()}
+    print('models.py / train_model / hyperparameters2: ', hyperparameters)
+
     # Instance GridSearchCV pour effectuer une recherche sur la grille d'hyperparamètres avec validation croisée
     # Possibilité d'ajuster le nombre de plis de validation croisée (cv) selon vos besoins
     grid_search = GridSearchCV(model, hyperparameters, cv=5)
@@ -28,17 +37,18 @@ def train_model(model, X_train, y_train, hyperparameters):
 def evaluate_model(model, X_test, y_test, model_family):
     # Faites des prédictions sur l'ensemble de test
     y_pred = model.predict(X_test)
+    round_val = 3
     if model_family == 'regression':
         # Métriques de régression (MSE, R²)
-        mse = mean_squared_error(y_test, y_pred)
-        r2 = r2_score(y_test, y_pred)
+        mse = round(mean_squared_error(y_test, y_pred), round_val)
+        r2 = round(r2_score(y_test, y_pred), round_val)
         metrics = {'MSE': mse, 'R²': r2}
     elif model_family == 'classification':
         # Métriques de classification (précision, rappel, F1-score, etc.)
-        accuracy = accuracy_score(y_test, y_pred)
-        precision = precision_score(y_test, y_pred)
-        recall = recall_score(y_test, y_pred)
-        f1 = f1_score(y_test, y_pred)
+        accuracy = round(accuracy_score(y_test, y_pred), round_val)
+        precision = round(precision_score(y_test, y_pred), round_val)
+        recall = round(recall_score(y_test, y_pred), round_val)
+        f1 = round(f1_score(y_test, y_pred), round_val)
         metrics = {'Accuracy': accuracy, 'Precision': precision, 'Recall': recall, 'F1-Score': f1}
     else:
         raise ValueError("Type de modèle non valide.")
