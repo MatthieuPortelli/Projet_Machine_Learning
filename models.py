6b463d8709dff0a752d8dfd32064d044c57c2863
matthieu <matthieu.portelli@gmail.com>
2023-09-06 10:49:59 +0200

    update: get hyperparameters to gridsearchcv from main.py

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, precision_score, recall_score, f1_score
 
 
-def train_model(model, X_train, y_train):
+def train_model(model, X_train, y_train, hyperparameters):
     if model in STRUCTURE['regression']:
         model_info = STRUCTURE['regression'][model]
     elif model in STRUCTURE['classification']:
@@ -11,13 +11,13 @@ def train_model(model, X_train, y_train):
     else:
         raise ValueError("Modèle non trouvé dans la structure.")
     model = model_info['model']
-    # Récupération des hyperparameters
-    hyperparameters = {param: param_info['values'] for param, param_info in model_info['hyperparameters'].items()}
     # Instance GridSearchCV pour effectuer une recherche sur la grille d'hyperparamètres avec validation croisée
     # Possibilité d'ajuster le nombre de plis de validation croisée (cv) selon vos besoins
     grid_search = GridSearchCV(model, hyperparameters, cv=5)
     # Entraîner le modèle avec les meilleurs hyperparamètres
+    print('models.py / train_model / avant .fit')
     grid_search.fit(X_train, y_train)
+    print('models.py / train_model / après .fit')
     # Récupérez le meilleur modèle, les meilleurs paramètres et le meilleur score
     best_model = grid_search.best_estimator_
     best_params = grid_search.best_params_
@@ -43,4 +43,4 @@ def evaluate_model(model, X_test, y_test, model_family):
     else:
         raise ValueError("Type de modèle non valide.")
     # Retournez les métriques
-    return metrics
+    return metrics, y_pred
