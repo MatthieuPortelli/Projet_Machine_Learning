81979263038b5a7cf6a752dca2b1d7cfcd1c9b02
matthieu <matthieu.portelli@gmail.com>
2023-09-07 11:26:21 +0200

    update: generate pdf file & explanations on metrics

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import db
 import constantes
+from report import generate_pdf_report
 from visualisations import visualize_selected_model
 from preprocess import DataPreprocessor
 from models import train_model, evaluate_model
+import base64
 
 
 def intro():
@@ -70,17 +72,38 @@ def sidebar():
                 st.write(f'<span style="color: #FFB923;"> {best_score}</span>', unsafe_allow_html=True)
             with st.expander("**Métriques**"):
                 metrics, y_pred = evaluate_model(best_model, X_test, y_test, model_family)
-                # Passer chaque métrique en orange
                 for metric_name, metric_value in metrics.items():
-                    st.markdown(f"{metric_name} : <span style='color: #FFB923'>{metric_value}</span>",
+                    st.markdown(f"<span style='color: #FFB923'>{metric_name}</span> : {metric_value}",
                                 unsafe_allow_html=True)
+                    if metric_name == "Accuracy":
+                        st.markdown("_Proportion de prédictions correctes parmi toutes les prédictions effectuées._")
+                    elif metric_name == "Precision":
+                        st.markdown("_Proportion de vrais positifs parmi toutes les prédictions positives._")
+                    elif metric_name == "Recall":
+                        st.markdown("_Proportion de vrais positifs parmi toutes les instances réelles positives._")
+                    elif metric_name == "F1-Score":
+                        st.markdown(
+                            "_Moyenne harmonique de la précision et du rappel, une mesure globale de la performance._")
+                    elif metric_name == "MSE":
+                        st.markdown(
+                            "_Mean Squared Error (Erreur quadratique moyenne), une mesure de la précision des prédictions._")
+                    elif metric_name == "R²":
+                        st.markdown(
+                            "_Coefficient de détermination R², une mesure de l'ajustement du modèle aux données._")
+                    else:
+                        st.markdown(f"{metric_name} : {metric_value}")
             with st.expander("**Visualisations**"):
                 fig_1, fig_2 = visualize_selected_model(selected_model, y_test, y_pred)
                 st.pyplot(fig_1)
                 st.pyplot(fig_2)
-            # # Dans votre fonction sidebar() ou toute autre fonction appropriée
-            # with st.expander("**Learning Curve**"):
-            #     plot_learning_curve(best_model, data_preprocessor.X_train, data_preprocessor.y_train)
+            # Gestion du pdf
+            generate_report = st.button("Générer le rapport PDF")
+            if generate_report:
+                pdf_data = generate_pdf_report(selected_model, best_params, best_score, metrics, fig_1, fig_2)
+                # Affichez le lien de téléchargement pour le PDF généré
+                st.success("Le rapport PDF a été généré avec succès. Cliquez ci-dessous pour le télécharger.")
+                st.markdown(get_binary_file_downloader_html(pdf_data, "Rapport", "Télécharger le PDF"),
+                            unsafe_allow_html=True)
         except Exception:
             st.sidebar.error(f"Il y a une erreur de sélection. Sélectionnez une ou plusieurs colonnes, colonne 'target' en dernière.")
 
@@ -109,26 +132,28 @@ def get_params(selected_algo):
     if on_test_size:
         test_size = st.sidebar.slider(" ", 0.5, 0.95, 0.2, 0.05)
     st.sidebar.markdown('---')
-    on_grid_search = st.sidebar.toggle("Désactiver GridSearchCV")
+    on_grid_search = st.sidebar.toggle("Désactiver GridSearchCV, modifier les hyperparamètres")
     if on_grid_search:
-        st.sidebar.write("Modifier les hyperparamètres")
         # Afficher les hyperparamètres selon le modèle choisi
         hyperparameters = constantes.get_hyperparameters(selected_algo)
         for parametre, values in hyperparameters.items():
             if all(isinstance(value, (int, float)) for value in values):
-                # print("isinstance: int, float")
                 selected_value = st.sidebar.select_slider(parametre, options=values['values'], key=parametre, help=values['description'])
             elif all(isinstance(value, bool) for value in values):
-                # print("isinstance: bool")
                 selected_value = st.sidebar.selectbox(parametre, options=values['values'], key=parametre, help=values['description'])
             else:
-                # print("isinstance: else (str)")
                 selected_value = st.sidebar.selectbox(parametre, options=values['values'], key=parametre, help=values['description'])
             # Ajouter le paramètre au dictionnaire
             selected_params[parametre] = [selected_value]
             # print("Paramètres sélectionnés :", selected_params[parametre])
     st.sidebar.markdown('---')
     return selected_params, test_size
+
+
+def get_binary_file_downloader_html(bin_data, file_label='File', btn_label='Download'):
+    b64 = base64.b64encode(bin_data).decode()
+    href = f'<a href="data:application/octet-stream;base64,{b64}" download="{file_label}.pdf">{btn_label}</a>'
+    return href
 
 
 def main():
