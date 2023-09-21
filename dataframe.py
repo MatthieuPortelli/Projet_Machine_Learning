2ba29ab35bcb47a47b68e18374441bf848431d86
Mattroc <darkmattroc@gmail.com>
2023-09-21 11:35:02 +0200

    add: handling csv files

---
+++ dataframe.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+# Fonction pour charger un fichier CSV et le transformer en DataFrame
+def load_csv_to_dataframe(uploaded_file):
+    df = pd.read_csv(uploaded_file)
+    # Supprimer la colonne "id" si elle existe
+    if "id" in df.columns:
+        df = df.drop(columns=["id"])
+    # Vérifier la présence d'une colonne "target"
+    if "target" not in df.columns:
+        return f"Votre dataset doit contenir une colonne 'target' pour entraîner et évaluer correctement votre modèle."
+    # Inspecter le type de données de la colonne "target"
+    target_type = df["target"].dtype
+    return df, target_type
